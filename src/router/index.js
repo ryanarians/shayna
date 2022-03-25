11e71d2a91c07c5526edc5f3fe9922940e23f31b
pc\HP <ryanarianto.s328@gmail.com>
2022-03-25 23:54:02 +0700

    Upload projek vue

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Product from '../views/Product.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
+import Success from '../views/SuccessShayna.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/product',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/success',
+    name: 'Success',
+    component: Success
+  },
+  {
+    path: '/cart',
+    name: 'ShoppingCart',
+    component: ShoppingCart
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenBannerShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomenBannerShayna from"@/components/WomenBannerShayna.vue";
+import InstaShayna from"@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+
+export default {
+  name: "Home",
+  components: {
+    HeroShayna,
+    HeaderShayna,
+    WomenBannerShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  },
+};
+</script>
